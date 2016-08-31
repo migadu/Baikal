edbfe5829cd95c4a67db0fb866b2c07d3c339ae3
Michael Bruderer <ojos@gmx.ch>
2016-08-31 09:46:18 +0200

    Add locks to postgres sql.

--- Core/Resources/Db/Postgres/db.sql
+++ Core/Resources/Db/Postgres/db.sql
@@ -148,6 +148,26 @@ CREATE TABLE schedulingobjects (
     size INTEGER NOT NULL
 );
 
+CREATE TABLE locks (
+    id SERIAL NOT NULL,
+    owner VARCHAR(100),
+    timeout INTEGER,
+    created INTEGER,
+    token VARCHAR(100),
+    scope SMALLINT,
+    depth SMALLINT,
+    uri TEXT
+);
+
+ALTER TABLE ONLY locks
+    ADD CONSTRAINT locks_pkey PRIMARY KEY (id);
+
+CREATE INDEX locks_token_ix
+    ON locks USING btree (token);
+
+CREATE INDEX locks_uri_ix
+ON locks USING btree (uri);
+
 CREATE TABLE principals (
     id SERIAL NOT NULL,
     uri VARCHAR(200) NOT NULL,
