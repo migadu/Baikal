1a4705063952979271c7dbc1e74ee2da945f7f8a
Michael Bruderer <ojos@gmx.ch>
2016-08-31 09:36:02 +0200

    Create table sql for postgres.

--- Core/Resources/Db/Postgres/db.sql
+++ Core/Resources/Db/Postgres/db.sql
@@ -1,134 +1,208 @@
 CREATE TABLE addressbooks (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    principaluri VARBINARY(255),
+    id SERIAL NOT NULL,
+    principaluri VARCHAR(255),
     displayname VARCHAR(255),
-    uri VARBINARY(200),
+    uri VARCHAR(200),
     description TEXT,
-    synctoken INT(11) UNSIGNED NOT NULL DEFAULT '1',
-    UNIQUE(principaluri(100), uri(100))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    synctoken INTEGER NOT NULL DEFAULT 1
+);
+ALTER TABLE ONLY addressbooks
+    ADD CONSTRAINT addressbooks_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX addressbooks_ukey
+    ON addressbooks USING btree (principaluri, uri);
 
 CREATE TABLE cards (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    addressbookid INT(11) UNSIGNED NOT NULL,
-    carddata MEDIUMBLOB,
-    uri VARBINARY(200),
-    lastmodified INT(11) UNSIGNED,
-    etag VARBINARY(32),
-    size INT(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    addressbookid INTEGER NOT NULL,
+    carddata BYTEA,
+    uri VARCHAR(200),
+    lastmodified INTEGER,
+    etag VARCHAR(32),
+    size INTEGER NOT NULL
+);
+
+ALTER TABLE ONLY cards
+    ADD CONSTRAINT cards_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX cards_ukey
+    ON cards USING btree (addressbookid, uri);
 
 CREATE TABLE addressbookchanges (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    uri VARBINARY(200) NOT NULL,
-    synctoken INT(11) UNSIGNED NOT NULL,
-    addressbookid INT(11) UNSIGNED NOT NULL,
-    operation TINYINT(1) NOT NULL,
-    INDEX addressbookid_synctoken (addressbookid, synctoken)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    uri VARCHAR(200) NOT NULL,
+    synctoken INTEGER NOT NULL,
+    addressbookid INTEGER NOT NULL,
+    operation SMALLINT NOT NULL
+);
+
+ALTER TABLE ONLY addressbookchanges
+    ADD CONSTRAINT addressbookchanges_pkey PRIMARY KEY (id);
+
+CREATE INDEX addressbookchanges_addressbookid_synctoken_ix
+ON addressbookchanges USING btree (addressbookid, synctoken);
+
 CREATE TABLE calendarobjects (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    calendardata MEDIUMBLOB,
-    uri VARBINARY(200),
-    calendarid INTEGER UNSIGNED NOT NULL,
-    lastmodified INT(11) UNSIGNED,
-    etag VARBINARY(32),
-    size INT(11) UNSIGNED NOT NULL,
-    componenttype VARBINARY(8),
-    firstoccurence INT(11) UNSIGNED,
-    lastoccurence INT(11) UNSIGNED,
-    uid VARBINARY(200),
-    UNIQUE(calendarid, uri)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    calendardata BYTEA,
+    uri VARCHAR(200),
+    calendarid INTEGER NOT NULL,
+    lastmodified INTEGER,
+    etag VARCHAR(32),
+    size INTEGER NOT NULL,
+    componenttype VARCHAR(8),
+    firstoccurence INTEGER,
+    lastoccurence INTEGER,
+    uid VARCHAR(200)
+);
+
+ALTER TABLE ONLY calendarobjects
+    ADD CONSTRAINT calendarobjects_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX calendarobjects_ukey
+    ON calendarobjects USING btree (calendarid, uri);
+
 
 CREATE TABLE calendars (
-    id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    principaluri VARBINARY(100),
-    displayname VARCHAR(100),
-    uri VARBINARY(200),
-    synctoken INTEGER UNSIGNED NOT NULL DEFAULT '1',
-    description TEXT,
-    calendarorder INT(11) UNSIGNED NOT NULL DEFAULT '0',
-    calendarcolor VARBINARY(10),
-    timezone TEXT,
-    components VARBINARY(21),
-    transparent TINYINT(1) NOT NULL DEFAULT '0',
-    UNIQUE(principaluri, uri)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    synctoken INTEGER NOT NULL DEFAULT 1,
+    components VARCHAR(21)
+);
 
-CREATE TABLE calendarchanges (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    uri VARBINARY(200) NOT NULL,
-    synctoken INT(11) UNSIGNED NOT NULL,
-    calendarid INT(11) UNSIGNED NOT NULL,
-    operation TINYINT(1) NOT NULL,
-    INDEX calendarid_synctoken (calendarid, synctoken)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ALTER TABLE ONLY calendars
+    ADD CONSTRAINT calendars_pkey PRIMARY KEY (id);
+
+
+CREATE TABLE calendarinstances (
+    id SERIAL NOT NULL,
+    calendarid INTEGER NOT NULL,
+    principaluri VARCHAR(100),
+    access SMALLINT NOT NULL DEFAULT '1', -- '1 = owner, 2 = read, 3 = readwrite'
+    displayname VARCHAR(100),
+    uri VARCHAR(200),
+    description TEXT,
+    calendarorder INTEGER NOT NULL DEFAULT 0,
+    calendarcolor VARCHAR(10),
+    timezone TEXT,
+    transparent SMALLINT NOT NULL DEFAULT '0',
+    share_href VARCHAR(100),
+    share_displayname VARCHAR(100),
+    share_invitestatus SMALLINT NOT NULL DEFAULT '2' --  '1 = noresponse, 2 = accepted, 3 = declined, 4 = invalid'
+);
+
+ALTER TABLE ONLY calendarinstances
+    ADD CONSTRAINT calendarinstances_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX calendarinstances_principaluri_uri
+    ON calendarinstances USING btree (principaluri, uri);
+
+
+CREATE UNIQUE INDEX calendarinstances_principaluri_calendarid
+    ON calendarinstances USING btree (principaluri, calendarid);
+
+CREATE UNIQUE INDEX calendarinstances_principaluri_share_href
+    ON calendarinstances USING btree (principaluri, share_href);
 
 CREATE TABLE calendarsubscriptions (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    uri VARBINARY(200) NOT NULL,
-    principaluri VARBINARY(100) NOT NULL,
+    id SERIAL NOT NULL,
+    uri VARCHAR(200) NOT NULL,
+    principaluri VARCHAR(100) NOT NULL,
     source TEXT,
     displayname VARCHAR(100),
     refreshrate VARCHAR(10),
-    calendarorder INT(11) UNSIGNED NOT NULL DEFAULT '0',
-    calendarcolor VARBINARY(10),
-    striptodos TINYINT(1) NULL,
-    stripalarms TINYINT(1) NULL,
-    stripattachments TINYINT(1) NULL,
-    lastmodified INT(11) UNSIGNED,
-    UNIQUE(principaluri, uri)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    calendarorder INTEGER NOT NULL DEFAULT 0,
+    calendarcolor VARCHAR(10),
+    striptodos SMALLINT NULL,
+    stripalarms SMALLINT NULL,
+    stripattachments SMALLINT NULL,
+    lastmodified INTEGER
+);
+
+ALTER TABLE ONLY calendarsubscriptions
+    ADD CONSTRAINT calendarsubscriptions_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX calendarsubscriptions_ukey
+    ON calendarsubscriptions USING btree (principaluri, uri);
+
+CREATE TABLE calendarchanges (
+    id SERIAL NOT NULL,
+    uri VARCHAR(200) NOT NULL,
+    synctoken INTEGER NOT NULL,
+    calendarid INTEGER NOT NULL,
+    operation SMALLINT NOT NULL DEFAULT 0
+);
+
+ALTER TABLE ONLY calendarchanges
+    ADD CONSTRAINT calendarchanges_pkey PRIMARY KEY (id);
+
+CREATE INDEX calendarchanges_calendarid_synctoken_ix
+    ON calendarchanges USING btree (calendarid, synctoken);
 
 CREATE TABLE schedulingobjects (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    principaluri VARBINARY(255),
-    calendardata MEDIUMBLOB,
-    uri VARBINARY(200),
-    lastmodified INT(11) UNSIGNED,
-    etag VARBINARY(32),
-    size INT(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-CREATE TABLE locks (
-    id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    owner VARCHAR(100),
-    timeout INTEGER UNSIGNED,
-    created INTEGER,
-    token VARBINARY(100),
-    scope TINYINT,
-    depth TINYINT,
-    uri VARBINARY(1000),
-    INDEX(token),
-    INDEX(uri(100))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    principaluri VARCHAR(255),
+    calendardata BYTEA,
+    uri VARCHAR(200),
+    lastmodified INTEGER,
+    etag VARCHAR(32),
+    size INTEGER NOT NULL
+);
+
 CREATE TABLE principals (
-    id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    uri VARBINARY(200) NOT NULL,
-    email VARBINARY(80),
-    displayname VARCHAR(80),
-    UNIQUE(uri)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    uri VARCHAR(200) NOT NULL,
+    email VARCHAR(80),
+    displayname VARCHAR(80)
+);
+
+ALTER TABLE ONLY principals
+    ADD CONSTRAINT principals_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX principals_ukey
+    ON principals USING btree (uri);
 
 CREATE TABLE groupmembers (
-    id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    principal_id INTEGER UNSIGNED NOT NULL,
-    member_id INTEGER UNSIGNED NOT NULL,
-    UNIQUE(principal_id, member_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    principal_id INTEGER NOT NULL,
+    member_id INTEGER NOT NULL
+);
+
+ALTER TABLE ONLY groupmembers
+    ADD CONSTRAINT groupmembers_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX groupmembers_ukey
+    ON groupmembers USING btree (principal_id, member_id);
+
+-- INSERT INTO principals (uri,email,displayname) VALUES
+-- ('principals/admin', 'admin@example.org','Administrator'),
+-- ('principals/admin/calendar-proxy-read', null, null),
+-- ('principals/admin/calendar-proxy-write', null, null);
 
 CREATE TABLE propertystorage (
-    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    path VARBINARY(1024) NOT NULL,
-    name VARBINARY(100) NOT NULL,
-    valuetype INT UNSIGNED,
-    value MEDIUMBLOB
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    path VARCHAR(1024) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    valuetype INT,
+    value BYTEA
+);
 
-CREATE UNIQUE INDEX path_property ON propertystorage (path(600), name(100));
+ALTER TABLE ONLY propertystorage
+    ADD CONSTRAINT propertystorage_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX propertystorage_ukey
+ON propertystorage (path, name);
+
 CREATE TABLE users (
-    id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARBINARY(50),
-    digesta1 VARBINARY(32),
-    UNIQUE(username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id SERIAL NOT NULL,
+    username VARCHAR(50),
+    digesta1 VARCHAR(32)
+);
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+CREATE UNIQUE INDEX users_ukey
+    ON users USING btree (username);
+
+-- INSERT INTO users (username,digesta1) VALUES
+-- ('admin', '87fd274b7b6c01e48d7c2f965da8ddf7');
